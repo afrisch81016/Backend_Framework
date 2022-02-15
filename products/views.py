0a74ab785e9bbb3f72561a7d34ed33d922fac4d4
Adam Frisch <afrisch88@outlook.com>
2022-02-15 14:10:30 -0600

    Made products to be utilized in the products table and fixed some syntax errors within the code with instructor help

--- products/views.py
+++ products/views.py
@@ -1,9 +1,16 @@
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import Productserializer
+from products import serializers
+
 
 @api_view(['GET'])
 def products_list(request):
 
+    products = Product.objects.all()
 
-    return Response ('ok')
+    serializer =Productserializer(products,many = True)
+
+    return Response(serializer.data)
