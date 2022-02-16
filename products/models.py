c13288babf95e072e8f7ac6d70c505abc406b243
Adam Frisch <afrisch88@outlook.com>
2022-02-16 09:47:04 -0600

    added the Delete function

--- products/models.py
+++ products/models.py
@@ -7,3 +7,4 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     inventory_quantity = models.IntegerField()
     price = models.DecimalField(max_digits=8,decimal_places=2)
+    # image_link =''
--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ from .serializers import Productserializer
 from products import serializers
 
 
-@api_view(['GET','POST','DELETE'])
+@api_view(['GET','POST'])
 def products_list(request,):
 
     if request.method == 'GET':
@@ -22,7 +22,7 @@ def products_list(request,):
         serializer.save()
         return Response (serializer.data,status = status.HTTP_201_CREATED)
 
-@api_view(['GET','PUT'])
+@api_view(['GET','PUT','DELETE'])
 def product_detail(request,pk):
     product = get_object_or_404(Product,pk=pk)
 
@@ -35,5 +35,9 @@ def product_detail(request,pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+
+        return Response (status = status.HTTP_204_NO_CONTENT)
 
 
